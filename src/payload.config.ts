e8af7267eb4d0f134e91a18aaf785d0f5c34cc7d
Umersiddiqui4 <umermun12@gmail.com>
2025-05-13 14:25:32 +0500

    Update serverURL configuration for production environment

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -21,7 +21,8 @@ dotenv.config();
 
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
+  // serverURL: 'http://localhost:3000',
+  serverURL: 'https://cms-coral-beta.vercel.app/',
   cors: [ "https://cap-sell.vercel.app"],
   // cors: ['http://localhost:3001', "https://cap-sell.vercel.app"],
   admin: {
