370ee772431a3ea72c3bd6c6be4c1863694c827c
Umersiddiqui4 <umermun12@gmail.com>
2025-05-16 17:05:18 +0500

    Refactor image field in CapCategory to restore original configuration and remove unnecessary hooks

--- src/collections/CapCategory.ts
+++ src/collections/CapCategory.ts
@@ -20,32 +20,11 @@ const CapCategory: CollectionConfig = {
       name: 'description',
       type: 'textarea',
     },
-    // {
-    //   name: 'image', // 👇 file/image field (choose one below)
-    //   type: 'upload',
-    //   relationTo: 'media', // 👈 make sure you have a media collection configured
-    //   required: false, // add this to avoid errors if media is missing
-    // },
-     {
-      name: 'uploadImage',
+    {
+      name: 'image', // 👇 file/image field (choose one below)
       type: 'upload',
-      relationTo: 'media', // You can remove this if not using media collection
-      hooks: {
-        beforeChange: [
-          async ({ value, originalDoc }) => {
-            if (value?.data) {
-              const uploaded = await cloudinary.uploader.upload(value.data, {
-                folder: 'caps',
-              })
-              return {
-                ...originalDoc,
-                imageUrl: uploaded.secure_url,
-              }
-            }
-            return value
-          },
-        ],
-      },
+      relationTo: 'media', // 👈 make sure you have a media collection configured
+      required: false, // add this to avoid errors if media is missing
     },
    
   ],
