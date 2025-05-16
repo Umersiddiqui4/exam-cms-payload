ce993c84d3531452a463c94cc1f3955ccadeab13
Umersiddiqui4 <umermun12@gmail.com>
2025-05-16 11:42:19 +0500

    Fix formatting and consistency in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from '@payloadcms/next/withPayload';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
-}
-module.exports = {
   swcMinify: false,
-}
+  // your other config here
+};
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig, { devBundleServerPackages: false });
