5da9edf9ee88015c3e6ac20e5f5e0f5a04755e23
Umersiddiqui4 <umermun12@gmail.com>
2025-05-17 16:11:42 +0500

    Refactor Media collection to remove unused fields and hooks for simplification

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,43 +1,12 @@
 import { CollectionConfig } from 'payload';
-import { uploadToCloudinary } from '../utils/cloudinary';
-import type { UploadApiResponse } from 'cloudinary';
 
 export const Media: CollectionConfig = {
   slug: 'media',
-  fields: [
-    {
-      name: 'url',
-      type: 'text',
-      required: true,
-    },
-    {
-      name: 'public_id',
-      type: 'text',
-    },
-  ],
-//   hooks: {
-//     beforeChange: [
-//       async ({ data, req }:any) => {
-//         const file = req.files?.file as {
-//           data: Buffer;
-//           name: string;
-//           mimetype: string;
-//         };
-
-//         if (file && file.data) {
-//           const filename = `media_${Date.now()}`;
-// console.log(file);
-
-//           const result: any = await uploadToCloudinary(file.data, filename);
-
-//           data.url = result.secure_url;
-//           data.public_id = result.public_id;
-
-//           delete data.file; // optional: prevent storing locally
-//         }
-
-//         return data;
-//       },
-//     ],
-//   },
+  upload: true,
+  admin: {
+    useAsTitle: 'filename',
+  },
+  fields: [],
 };
+
+
